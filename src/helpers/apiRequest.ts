497af039ba28372ff6752ed464a675874ed91bff
Seggi SMS <seggimarugira@gmail.com>
2022-09-02 20:14:21 +0200

    add axios config

---
+++ src/helpers/apiRequest.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import axios from "axios";
+
+const { URL_API } = process.env;
+const contentType: {} = { "Content-Type": "application/json" };
+const defaultUrls: {apiV1: any} = { apiV1: "/api/v1" };
+
+const api = axios.create({
+  baseURL: URL_API,
+  headers: contentType,
+});
+
+interface RequestOptions {
+    method: string,
+    api: any,
+    body: any
+}
+
+const apiRequest = async (requestMethod: string, url: any, data: any = null) => {
+  const requestOptions: RequestOptions = {
+    api,
+    method: requestMethod,
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch(`${defaultUrls.apiV1}/${url}`, requestOptions);
+    return response.json();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default apiRequest;

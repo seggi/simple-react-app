c4e0074ce4c4f70ece23856645fd470a567e47c5
Seggi SMS <seggimarugira@gmail.com>
2022-09-02 21:01:26 +0200

    final redux configuration

---
+++ src/redux/reducers.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+// import getProductList from "./reducers/getProductList";
+
+const allReducers = combineReducers({
+//   userReducer: getProductList,
+});
+
+export default allReducers;
